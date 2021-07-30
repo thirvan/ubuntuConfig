74c632c68e56d05a37a316db1e115ed835e423d8
thirvan <thirvansomanah@gmail.com>
2021-07-30 13:42:28 +0400

    replace cp with mv to make backup of config files

--- link.sh
+++ link.sh
@@ -1,10 +1,10 @@
 mkdir ~/configBAK
 
 #copy existing config files to configBAK
-cp ~/.bashrc ~/configBAK/.bashrc.bak
-cp ~/.bash_aliases ~/configBAK/.bash_aliases.bak
-cp ~/.bash_profile ~/configBAK/.bash_profile.bak
-cp ~/.vimrc ~/configBAK/.vimrc.bak
+mv ~/.bashrc ~/configBAK/.bashrc.bak
+mv ~/.bash_aliases ~/configBAK/.bash_aliases.bak
+mv ~/.bash_profile ~/configBAK/.bash_profile.bak
+mv ~/.vimrc ~/configBAK/.vimrc.bak
 
 ln -s $(pwd)/.bashrc ~/.
 ln -s $(pwd)/.bash_aliases ~/.
