b9349c81fd9de54acb982758c54f53a5b2077a64
thirvan <thirvansomanah@gmail.com>
2021-09-21 18:46:56 +0400

    add alias to cd to fintech repo

--- link.sh
+++ link.sh
@@ -3,10 +3,12 @@ mkdir ~/configBAK
 #copy existing config files to configBAK
 mv ~/.bashrc ~/configBAK/.bashrc.bak
 mv ~/.bash_aliases ~/configBAK/.bash_aliases.bak
+mv ~/.tmp_aliases ~/configBAK/.tmp_aliases.bak
 mv ~/.bash_profile ~/configBAK/.bash_profile.bak
 mv ~/.vimrc ~/configBAK/.vimrc.bak
 
 ln -s $(pwd)/.bashrc ~/.
 ln -s $(pwd)/.bash_aliases ~/.
+ln -s $(pwd)/.tmp_aliases ~/.
 ln -s $(pwd)/.bash_profile ~/.
 ln -s $(pwd)/.vimrc ~/.
