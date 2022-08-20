33e2d548e7124e40076a2eb504b7f03a97431bf8
Thirvan VM <thirvansomanah@gmail.com>
2022-08-20 11:50:42 +0400

    Add simple script to install basic software

---
+++ downloads.sh
@@ -0,0 +1,10 @@
+sudo apt update && sudo apt upgrade
+sudo apt install git
+sudo apt install vim
+sudo apt install bat
+sudo apt install openssh-server
+sudo apt install gcc
+sudo apt install net-tools
+sudo apt install xsel
+sudo snap install code --classic
+
